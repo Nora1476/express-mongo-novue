0fcd86114ff341063a99da8ec0066b818de6c78c
Nora1476 <nada70979@gmail.com>
2022-06-11 23:52:10 +0900

    set1

--- schemas/index.js
+++ schemas/index.js
@@ -1,27 +1,25 @@
-//mongodvb에 접근하기 위한 틀
-
-const mongoose = require("mongoose")
-const connect = function(){
-    if(process.env.NODE_ENV !== 'production'){
-        mongoose.set('debug',true)
+const mongoose = require('mongoose')
+const connect = function () {
+  if (process.env.NODE_ENV !== 'production') {
+    mongoose.set('debug', true)
+  }
+  // mongoose.connect('mongodb://root:1234@127.0.0.1:27017/admin',
+  mongoose.connect('mongodb://mango:1234@localhost:27017/sensor',
+    { dbName: 'sensor', useNewUrlParser: true, },
+    function (error) {
+      if (error) {
+        console.log('mongodb connection failed')
+      } else {
+        console.log('mongodb connection success')
+      }
     }
-    mongoose.connect('mongodb://sensor:1234@localhost:27017/admin',
-        {dbName:'sensor',useNewUrlParser:true, useCreateIndex:true,},
-        function(err){
-            if(err){
-                console.log('mongodb connection failed');
-            }else{
-                console.log('mongodb connection success');
-            }
-        }
-    )
+  )
 }
-mongoose.connection.on('error',function(error){
-    console.log('mongodb connection error', error);
+mongoose.connection.on('error', function (error) {
+  console.error('mongodb connection error', error)
 })
-mongoose.connection.on('disconnected ',function(error){
-    console.log('mongodb is disconnected retry connection', error)
-    connect();
+mongoose.connection.on('disconnected', function () {
+  console.error('mongodb is disconnected. retry connection')
+  connect()
 })
-
-module.exports=connect
+module.exports = connect
