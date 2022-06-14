7db9a2d2a6f495fddac020ca6cfea673fa55b0ab
Nora1476 <nada70979@gmail.com>
2022-06-14 23:53:31 +0900

    add comment

--- schemas/index.js
+++ schemas/index.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose')
 
 
 const connect = function () {
+  // 개발 환경일 때만 콘솔을 통해 몽구스가 생성하는 쿼리 내용 확인
   if (process.env.NODE_ENV !== 'production') {
     mongoose.set('debug', true)                       // 몽고 쿼리가 콘솔에서 뜨게 한다.
   }
+
+   // 몽구스와 몽고디비 연결하는 부분
   // 기본요청은 ('mongodb://root:1234@127.0.0.1:27017/admin') 처음몽구스 설치할때 admin 값으로 준 id와 pw
   // 일반사용자로 등록했던 use:sensor의 id:pass@기본포트 연결 설정
   mongoose.connect('mongodb://mango:1234@localhost:27017/sensor', { 
